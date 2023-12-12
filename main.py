e3161a0da3394b46435858b7476f090ca651d40d
sdf299 <quocbao0905461606@gmail.com>
2023-12-12 10:36:38 +0700

    Set debug=False

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -297,4 +297,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False)
